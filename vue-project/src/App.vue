57989c15fb8321e9d36d0571f2c383034d8b38c9
Dineo Mathibela <dineomathibela161@gmail.com>
2024-10-09 16:38:28 +0200

    clean: delete unused files

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <div class="bg-red-200">Hello World</div>
+    <div class="bg-red-200">Hello World, part 2</div>
   </header>
 
   <!-- <RouterView /> -->
